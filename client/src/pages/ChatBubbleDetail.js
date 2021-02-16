dc479d20cb3fd93cad1ffd9fdafcabf3e0b25620
Victoria Lee <victoriacmlee@gmail.com>
2021-02-15 23:27:28 -0500

    setup array

--- client/src/pages/ChatBubbleDetail.js
+++ client/src/pages/ChatBubbleDetail.js
@@ -3,13 +3,37 @@ import React, { useEffect, useState } from "react";
 import BubbleDetail from "../components/bubbleDetail"
 import BubbleResponse from "../components/bubbleResponse"
 
+import words from "../utils/badWordArray"
+
 function ChatBubbleDetail() {
+
+
+    const input = "hey you are a whore";
+
+    function handleInput() {
+
+        if (words.some(word => input.toLowerCase().includes(word))) {
+            const arr = []
+            for (var i = 0; i <= words.length; i++) {
+                
+                if (input.toLowerCase().includes(words[i])) {
+                    arr.push(words[i])
+                }
+            }
+            console.log(arr)
+        }
+        else {
+
+            console.log("no bad words detected")
+        }
+    };
+
 
     return (
         <div>
 
             <BubbleDetail />
-            <BubbleResponse/>
+            <BubbleResponse />
             <input placeholder="Respond to bubble..."></input>
 
             <button>Send Button</button>
